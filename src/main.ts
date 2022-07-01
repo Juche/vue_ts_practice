ee13263a174fef190c0f2c58b5839b83e4429041
Juching <azhucheng1@qq.com>
2022-07-01 11:40:31 +0800

    wip: update

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import Model from './Model.vue'
 
-// NOTE: [基础.01][创建一个应用]
+// NOTE: [基础.01] [创建一个应用]
 const app = createApp(App)  // App 作为根组件
 
 // 应用实例会暴露一个 .config 对象允许我们配置一些应用级的选项
@@ -16,5 +16,5 @@ app.mount('#app')  // #app => index.html 里的根节点
 // createApp 创建多应用实例
 // createApp API 允许多个 Vue 应用共存于同一个页面上
 // 而且每个应用都拥有自己的用于配置和全局资源的作用域
-const model = createApp(Model)
-model.mount('#model')
+// const model = createApp(Model)
+// model.mount('#model')
