1d29e52fbc2902755df8eecaf751f6e574c5f680
Juching <azhucheng1@qq.com>
2022-07-01 17:17:54 +0800

    feat: [基础.02] [模板语法]

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
-  <h1>App Header</h1>
+  <!-- <h1>App Header</h1> -->
+  <V102 />
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+  import V102 from './components/V102.vue';
+</script>
 
 <style scoped></style>
---
+++ src/components/V102.vue
@@ -0,0 +1,74 @@
+<template>
+  <h2>1. “Mustache”语法</h2>
+  <p>{{ user.name }} - {{ user.age }} - {{ user?.fav ?? '无' }}</p>
+  <h2>2. Attribute 绑定</h2>
+  <!-- Attribute 绑定 -->
+  <button :disabled="disabled">按钮</button>
+  <button @click="toggleState">toggleState</button>
+  <!-- 动态绑定多个 Attribute -->
+  <div v-bind="attrs">我是一个方块快</div>
+  <h2>3. 指令</h2>
+  <div :title="evType" :[attrName]="attrVal" @[evType]="enHandle">我绑定了动态属性</div>
+  <button @click="changeEvType">我修改动态属性</button>
+</template>
+
+<script lang="ts" setup>
+  import { reactive, ref } from 'vue';
+
+  const user = reactive({
+    name: 'Juching',
+    age: 18,
+    fav: null,
+  });
+
+  const disabled = ref(false);
+  const attrs = reactive({
+    class: 'rect rect-red',
+    id: 'rect1',
+  });
+
+  function toggleState() {
+    disabled.value = !disabled.value;
+  }
+
+  const attrName = ref('class');
+  const attrVal = ref('');
+  const evType = ref('click');
+  const count = ref(0);
+
+  const classList = ['red', 'green', 'blue'];
+  function enHandle() {
+    attrVal.value = classList[count.value % 3];
+  }
+
+  const evList = ['click', 'mouseenter', 'dblclick'];
+  function changeEvType() {
+    count.value++;
+    evType.value = evList[count.value % 3];
+  }
+</script>
+
+<style scoped>
+  .rect {
+    margin: 20px;
+    width: 200px;
+    height: 100px;
+  }
+  .rect-red {
+    background: #f00;
+  }
+
+  #rect1 {
+    border: 1px solid #000;
+  }
+
+  .red {
+    color: red;
+  }
+  .green {
+    color: green;
+  }
+  .blue {
+    color: blue;
+  }
+</style>
